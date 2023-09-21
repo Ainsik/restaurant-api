a567bbba7ecf29f421bcc6cfff6a627bebd81293
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-21 18:46:03 +0200

    feat: create restaurant service and implement GetAll method

---
+++ src/Aplication/Contracts/Application/IRestaurantService.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dto.Restaurant;
+
+namespace Application.Contracts.Application;
+public interface IRestaurantService
+{
+    Task<IEnumerable<RestaurantDto>> GetAllAsync();
+    Task<RestaurantDto> GetByIdAsync(int id);
+    Task<(int, NewRestaurantDto)> CreateAsync(NewRestaurantDto dto);
+    Task UpdateAsync(int id, UpdateRestaurantDto dto);
+    Task DeleteAsync(int id);
+}
---
+++ src/Aplication/Services/RestaurantService.cs
@@ -0,0 +1,47 @@
+﻿using Application.Contracts.Application;
+using Application.Contracts.Infrastructure;
+using Application.Dto.Restaurant;
+using AutoMapper;
+
+namespace Application.Services;
+public class RestaurantService : IRestaurantService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public RestaurantService(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<RestaurantDto>> GetAllAsync()
+    {
+        var restaurants = await _unitOfWork.RestaurantRepository
+            .GetAllAsync(includeProperties: "Address, Dishes");
+
+        var restaurantsDto = _mapper.Map<List<RestaurantDto>>(restaurants);
+
+        return restaurantsDto;
+    }
+
+    public async Task<RestaurantDto> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<(int, NewRestaurantDto)> CreateAsync(NewRestaurantDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(int id, UpdateRestaurantDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
