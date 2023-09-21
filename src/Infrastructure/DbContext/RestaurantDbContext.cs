f98842577a473e01eecd92873608011a6410e76b
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-21 11:17:44 +0200

    feat: implement db context

---
+++ src/Infrastructure/DbContext/RestaurantDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DbContext;
+public class RestaurantDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
