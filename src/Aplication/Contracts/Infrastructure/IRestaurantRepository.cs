72b76ab93b8c0cc3a7cf9562ef62a7e4e63dbc19
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-21 18:43:44 +0200

    feat: implement restaurant repository

---
+++ src/Aplication/Contracts/Infrastructure/IRestaurantRepository.cs
@@ -0,0 +1,7 @@
+﻿using Core.Entities;
+
+namespace Application.Contracts.Infrastructure;
+public interface IRestaurantRepository : IRepository<Restaurant>
+{
+
+}
---
+++ src/Infrastructure/Repositories/RestaurantRepository.cs
@@ -0,0 +1,11 @@
+﻿using Application.Contracts.Infrastructure;
+using Core.Entities;
+
+namespace Infrastructure.Repositories;
+public class RestaurantRepository : Repository<Restaurant>, IRestaurantRepository
+{
+    public RestaurantRepository(Microsoft.EntityFrameworkCore.DbContext context) : base(context)
+    {
+        
+    }
+}
