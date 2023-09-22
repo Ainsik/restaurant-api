94ef65819480589a9a17b0c30b6c712f8f62c5fb
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-22 18:03:02 +0200

    feat: implement validators

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,9 +1,16 @@
 using Application.Contracts.Application;
 using Application.Contracts.Infrastructure;
+using Application.Dto.Address;
+using Application.Dto.Dish;
+using Application.Dto.Restaurant;
 using Application.Profiles;
 using Application.Services;
+using FluentValidation;
 using Infrastructure.DbContext;
 using Infrastructure.Repositories;
+using Infrastructure.Validations.Address;
+using Infrastructure.Validations.Dish;
+using Infrastructure.Validations.Restaurant;
 using Microsoft.EntityFrameworkCore;
 using Polly;
 
@@ -17,6 +24,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<RestaurantDbContext>(
     option => option.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantConnectionString"))
 );
+
+builder.Services.AddScoped<IValidator<NewAddressDto>, NewAddressDtoValidator>();
+builder.Services.AddScoped<IValidator<UpdateAddressDto>, UpdateAddressDtoValidator>();
+
+builder.Services.AddScoped<IValidator<NewRestaurantDto>, NewRestaurantDtoValidator>();
+builder.Services.AddScoped<IValidator<UpdateRestaurantDto>, UpdateRestaurantDtoValidator>();
+
+builder.Services.AddScoped<IValidator<NewDishDto>, NewDishDtoValidator>();
+builder.Services.AddScoped<IValidator<UpdateDishDto>, UpdateDishDtoValidator>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
