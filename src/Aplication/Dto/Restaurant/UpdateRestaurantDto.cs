ba360d491b4ea596a57aa69d8ef5c96ee7289928
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-22 18:17:11 +0200

    refactor: udapte restaurant dto

--- src/Aplication/Dto/Restaurant/UpdateRestaurantDto.cs
+++ src/Aplication/Dto/Restaurant/UpdateRestaurantDto.cs
@@ -1,5 +1,7 @@
 ﻿namespace Application.Dto.Restaurant;
 public class UpdateRestaurantDto
 {
-
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool HasDelivery { get; set; }
 }
