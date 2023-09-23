a06e17723919735c06521a5af4d24c951cf569f1
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-23 19:40:15 +0200

    refactor: address validator

--- src/Infrastructure/Validations/Address/NewAddressDtoValidator.cs
+++ src/Infrastructure/Validations/Address/NewAddressDtoValidator.cs
@@ -1,11 +1,22 @@
 ﻿using Application.Dto.Address;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace Infrastructure.Validations.Address;
 public class NewAddressDtoValidator : AbstractValidator<NewAddressDto>
 {
     public NewAddressDtoValidator()
     {
-        
+        RuleFor(a => a.City)
+            .NotEmpty().WithMessage("City is required.")
+            .MaximumLength(50).WithMessage("City can't be longer than 50 characters.");
+
+        RuleFor(a => a.Street)
+            .NotEmpty().WithMessage("City is required.")
+            .MaximumLength(50).WithMessage("City can't be longer than 50 characters.");
+
+        RuleFor(a => a.PostalCode)
+            .Matches(new Regex("\\d{2}-\\d{3}"))
+            .WithMessage("Invalid postal code format.");
     }
 }
--- src/Infrastructure/Validations/Address/UpdateAddressDtoValidator.cs
+++ src/Infrastructure/Validations/Address/UpdateAddressDtoValidator.cs
@@ -1,4 +1,5 @@
-﻿using Application.Dto.Address;
+﻿using System.Text.RegularExpressions;
+using Application.Dto.Address;
 using FluentValidation;
 
 namespace Infrastructure.Validations.Address;
@@ -6,6 +7,16 @@ public class UpdateAddressDtoValidator : AbstractValidator<UpdateAddressDto>
 {
     public UpdateAddressDtoValidator()
     {
-        
+        RuleFor(a => a.City)
+            .NotEmpty().WithMessage("City is required.")
+            .MaximumLength(50).WithMessage("City can't be longer than 50 characters.");
+
+        RuleFor(a => a.Street)
+            .NotEmpty().WithMessage("City is required.")
+            .MaximumLength(50).WithMessage("City can't be longer than 50 characters.");
+
+        RuleFor(a => a.PostalCode)
+            .Matches(new Regex("\\d{2}-\\d{3}"))
+            .WithMessage("Invalid postal code format.");
     }
 }
