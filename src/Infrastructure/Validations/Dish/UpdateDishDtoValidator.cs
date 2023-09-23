c2a3affc3f1187aca81fcd76fd4fa1344339b83d
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-23 19:48:16 +0200

    refactor: dish validation

--- src/Infrastructure/Validations/Dish/UpdateDishDtoValidator.cs
+++ src/Infrastructure/Validations/Dish/UpdateDishDtoValidator.cs
@@ -6,6 +6,15 @@ public class UpdateDishDtoValidator : AbstractValidator<UpdateDishDto>
 {
     public UpdateDishDtoValidator()
     {
-        
+        RuleFor(d => d.Name)
+            .NotEmpty().WithMessage("Dish name is required.")
+            .MaximumLength(50).WithMessage("Dish name can't be longer than 50 characters.");
+
+        RuleFor(d => d.Description)
+            .NotEmpty().WithMessage("Dish description is required.")
+            .MaximumLength(200).WithMessage("Dish description can't be longer than 200 characters.");
+
+        RuleFor(d => d.Price)
+            .NotEmpty().WithMessage("Dish price is required.");
     }
 }
