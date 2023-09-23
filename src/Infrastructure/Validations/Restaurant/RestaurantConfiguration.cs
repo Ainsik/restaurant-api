431f86244595e09537a3ffca49ffb0acc2d69ef7
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-23 18:49:38 +0200

    refactor: restaurant configuration

--- src/Infrastructure/Validations/Restaurant/RestaurantConfiguration.cs
+++ src/Infrastructure/Validations/Restaurant/RestaurantConfiguration.cs
@@ -6,6 +6,31 @@ public class RestaurantConfiguration : IEntityTypeConfiguration<Core.Entities.Re
 {
     public void Configure(EntityTypeBuilder<Core.Entities.Restaurant> builder)
     {
-        throw new NotImplementedException();
+        builder.Property(r => r.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(r => r.Description)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(r => r.Category)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(r => r.HasDelivery)
+            .IsRequired();
+
+        builder.Property(r => r.ContactEmail)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasIndex(r => r.ContactEmail)
+            .IsUnique();
+
+        builder.Property(r => r.ContactNumber)
+            .IsRequired()
+            .HasMaxLength(9)
+            .IsFixedLength();
     }
 }
