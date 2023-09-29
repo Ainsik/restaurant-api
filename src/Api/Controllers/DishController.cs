cfa643ff0699176b2e542178b73e61b89a31c826
Ainsik <pio.andrzejczuk@gmail.com>
2023-09-29 20:39:51 +0200

    feat: implement dish controller with create new dish enpoint

---
+++ src/Api/Controllers/DishController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts.Application;
+using Application.Dto.Dish;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("/api/restaurant/{restaurantId}/dish")]
+public class DishController : ControllerBase
+{
+    private readonly IDishService _dishService;
+
+    public DishController(IDishService dishService)
+    {
+        _dishService = dishService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateDishAsync([FromRoute] int restaurantId,[FromBody] NewDishDto dto)
+    {
+        await _dishService.CreateAsync(restaurantId, dto);
+
+        return Ok();
+    }
+}
