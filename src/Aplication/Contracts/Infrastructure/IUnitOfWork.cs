4eb1dd71d4f192868b03d72b5224addd9726b072
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-01 21:05:45 +0200

    feat: register user repository

--- src/Aplication/Contracts/Infrastructure/IUnitOfWork.cs
+++ src/Aplication/Contracts/Infrastructure/IUnitOfWork.cs
@@ -3,5 +3,6 @@ public interface IUnitOfWork : IDisposable
 {
     IRestaurantRepository RestaurantRepository { get; }
     IDishRepository DishRepository { get; }
+    IUserRepository UserRepository { get; }
     Task SaveAsync();
 }
