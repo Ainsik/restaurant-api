0cbf1ccbe321ab461577cbaf5461da17b59ba331
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-01 13:07:33 +0200

    feat: role and user configuration

---
+++ src/Infrastructure/Validations/Role/RoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Validations.Role;
+public class RoleConfiguration : IEntityTypeConfiguration<Core.Entities.Role>
+{
+    public void Configure(EntityTypeBuilder<Core.Entities.Role> builder)
+    {
+        builder.HasMany(r => r.Users)
+            .WithOne(u => u.Role)
+            .HasForeignKey(u => u.RoleId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(r => r.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
---
+++ src/Infrastructure/Validations/User/UserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Validations.User;
+public class UserConfiguration : IEntityTypeConfiguration<Core.Entities.User>
+{
+    public void Configure(EntityTypeBuilder<Core.Entities.User> builder)
+    {
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.Nationality)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
