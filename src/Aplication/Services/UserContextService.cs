81920607756e8a808f43282f808a6a1c7d107f57
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-05 22:11:37 +0200

    refactor: user context service

--- src/Aplication/Services/UserContextService.cs
+++ src/Aplication/Services/UserContextService.cs
@@ -13,8 +13,8 @@ public class UserContextService : IUserContextService
         _httpContextAccessor = httpContextAccessor;
     }
 
-    public ClaimsPrincipal? User => _httpContextAccessor.HttpContext?.User;
+    public ClaimsPrincipal User => _httpContextAccessor.HttpContext.User;
 
     public int? GetUserId
-        => User is null ? null : int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+        => int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
 }
