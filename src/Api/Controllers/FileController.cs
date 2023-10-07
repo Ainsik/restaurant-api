3d178ba88b6501d9e7a0753bf7e470d12eb01f4c
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-07 18:41:26 +0200

    feat: create ability to download static files

---
+++ src/Api/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("file")]
+[Authorize]
+public class FileController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult> GetFile([FromQuery] string fileName)
+    {
+        var rootPath = Directory.GetCurrentDirectory();
+        var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+        var fileExists = System.IO.File.Exists(filePath);
+
+        if (!fileExists)
+        {
+            return NotFound();
+        }
+
+        var contentTypeProvider = new FileExtensionContentTypeProvider();
+        contentTypeProvider.TryGetContentType(fileName, out var contentType);
+
+        var fileContents = await System.IO.File.ReadAllBytesAsync(filePath);
+
+        return File(fileContents, contentType, fileName);
+    }
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -143,6 +143,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
+
 app.UseCors();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
