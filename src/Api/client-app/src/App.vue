dd7e2b9e7e6cd56d92482ab3fc65fd12520d5b9b
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-08 12:25:21 +0200

    feat: implement get restaurant by id

--- src/Api/client-app/src/App.vue
+++ src/Api/client-app/src/App.vue
@@ -1,7 +1,43 @@
 <template>
-	<div>Hi</div>
+	<section>
+		<div>
+			<h2>CURRENT ID: {{ restaurantId }}</h2>
+			<input type="text" v-model="restaurantId" />
+			<button @click="getRestaurant">getRestaurant</button>
+			<p v-if="isValid">
+				ID: {{ restaurant.id }} <br />
+				Name: {{ restaurant.name }}
+			</p>
+			<p v-else>Invalid id!</p>
+		</div>
+	</section>
 </template>
 
-<script></script>
+<script>
+import axios from "axios";
+const API = "https://localhost:7236/api/restaurant";
+export default {
+	data() {
+		return {
+			restaurant: {},
+			restaurantId: null,
+		};
+	},
+	methods: {
+		async getRestaurant() {
+			this.isValid = true;
+			try {
+				await axios.get(`${API}/${this.restaurantId}`).then((result) => {
+					const { data } = result;
+					this.restaurant = data;
+				});
+			} catch (error) {
+				this.isValid = false;
+			}
+			this.restaurantId = null;
+		},
+	},
+};
+</script>
 
 <style></style>
