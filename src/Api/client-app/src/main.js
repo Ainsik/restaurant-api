673ac25af60c6fc0c9a35e96851d5c17c9cf58a8
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-09 13:45:18 +0200

    fix: store

--- src/Api/client-app/src/main.js
+++ src/Api/client-app/src/main.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import store from "./store";
+import store from "./store/index.js";
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
