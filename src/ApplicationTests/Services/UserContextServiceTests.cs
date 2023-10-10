9c7ceab05c9d8c84272157eaa8c947e1548c0baf
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-10 19:08:21 +0200

    test: implement GetUserId_WithAuthenticatedUser_ShouldReturnUserId

---
+++ src/ApplicationTests/Services/UserContextServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using Application.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Xunit;
+
+namespace ApplicationTests.Services;
+public class UserContextServiceTests
+{
+    [Fact]
+    public void GetUserId_WithAuthenticatedUser_ShouldReturnUserId()
+    {
+        //arrange
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, "1"),
+            new(ClaimTypes.Name, "User"),
+            new(ClaimTypes.Role, "User")
+        };
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims));
+
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+
+        httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+        {
+            User = user
+        });
+
+        var userContext = new UserContextService(httpContextAccessorMock.Object);
+
+        //act
+        var userId = userContext.GetUserId;
+
+        //arrange
+        userId.Should().Be(1);
+    }
+}
