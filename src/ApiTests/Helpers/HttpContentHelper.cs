044ec2b09b5db2f59ea867a4afd2db24c7cb0c31
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-14 22:51:39 +0200

    test: create new restaurant tests

---
+++ src/ApiTests/Helpers/HttpContentHelper.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiTests.Helpers;
+public static class HttpContentHelper
+{
+    public static HttpContent ToJsonHttpContent(this object obj)
+    {
+        var json = JsonConvert.SerializeObject(obj);
+
+        var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+        return httpContent;
+    }
+}
