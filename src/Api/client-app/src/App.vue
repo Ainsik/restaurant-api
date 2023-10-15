cf6e3d254239d5ba874abd08626a671f33df381d
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-15 16:09:29 +0200

    refactor: get single restaurant by id

--- src/Api/client-app/src/App.vue
+++ src/Api/client-app/src/App.vue
@@ -1,51 +1,8 @@
 <template>
 	<router-view />
-	<section>
-		<div>
-			<h2>CURRENT ID: {{ restaurantId }}</h2>
-			<input type="text" v-model="restaurantId" />
-			<button @click="getRestaurant">getRestaurant</button>
-			<p>
-				ID: {{ restaurant.id }} <br />
-				Name: {{ restaurant.name }}
-			</p>
-		</div>
-	</section>
 </template>
 
-<script>
-import axios from "axios";
-const API = "https://localhost:7236/api/restaurant";
-export default {
-	data() {
-		return {
-			restaurant: {},
-			restaurantId: null,
-		};
-	},
-	methods: {
-		async getRestaurant() {
-			const token = this.$store.getters.getToken;
-
-			await axios
-				.get(`${API}/${this.restaurantId}`, {
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
-				})
-				.then((result) => {
-					const { data } = result;
-					this.restaurant = data;
-				})
-				.catch((error) => {
-					console.error("Błąd żądania GET", error.response);
-				});
-
-			this.restaurantId = null;
-		},
-	},
-};
-</script>
+<script></script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
--- src/Api/client-app/src/pages/AllRestaurants.vue
+++ src/Api/client-app/src/pages/AllRestaurants.vue
@@ -1,11 +1,47 @@
 <template>
 	<section>
-		<div>all restaurants</div>
-		<div>all restaurants</div>
-		<div>all restaurants</div>
-		<div>all restaurants</div>
-		<div>all restaurants</div>
-		<div>all restaurants</div>
-		<div>all restaurants</div>
+		<div class="container">
+			<h2>CURRENT ID: {{ restaurantId }}</h2>
+			<input type="text" v-model="restaurantId" />
+			<button @click="getRestaurant">getRestaurant</button>
+			<p>
+				ID: {{ restaurant.id }} <br />
+				Name: {{ restaurant.name }}
+			</p>
+		</div>
 	</section>
 </template>
+
+<script>
+import axios from "axios";
+const API = "https://localhost:7236/api/restaurant";
+export default {
+	data() {
+		return {
+			restaurant: {},
+			restaurantId: null,
+		};
+	},
+	methods: {
+		async getRestaurant() {
+			const token = this.$store.getters.getToken;
+
+			await axios
+				.get(`${API}/${this.restaurantId}`, {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				})
+				.then((result) => {
+					const { data } = result;
+					this.restaurant = data;
+				})
+				.catch((error) => {
+					console.error("Błąd żądania GET", error.response);
+				});
+
+			this.restaurantId = null;
+		},
+	},
+};
+</script>
