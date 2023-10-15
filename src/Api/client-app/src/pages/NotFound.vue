c66920abae5240ae8d5f7cfc09ac74627c80ad4f
Ainsik <pio.andrzejczuk@gmail.com>
2023-10-15 16:57:09 +0200

    feat: create not found page

---
+++ src/Api/client-app/src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+	<section>
+		<div class="container text-center">
+			<h2 class="m-5">Page not found</h2>
+			<p>
+				This page could not be found - maybe check out our
+				<router-link to="/">home page.</router-link>.
+			</p>
+		</div>
+	</section>
+</template>
